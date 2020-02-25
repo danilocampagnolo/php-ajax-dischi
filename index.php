55613f52e47dda8021e37060fd97a90bb4ce7a38
Danilo <danilo.campagnolo.93@gmail.com>
2020-02-25 16:16:00 +0100

    start css

--- index.php
+++ index.php
@@ -6,20 +6,31 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
     <title></title>
   </head>
   <body>
-    <ul>
-      <?php foreach ($database as $value): ?>
-      <li> Title: <?php echo $value["title"] ?> </li>
-        <ul>
-          <li> Author: <?php echo $value["author"] ?> </li>
-          <li> Year: <?php echo $value["year"] ?> </li>
-          <li><img src="<?php echo $value["poster"] ?>" alt=""> </li>
-        </ul>
-      </li>
-      <?php endforeach; ?>
-    </ul>
+    <header>
+      <div class="wrapper">
+      </div>
+    </header>
+    <main>
+      <div class="wrapper">
+        <div class="disks">
+          <?php foreach ($database as $value): ?>
+            <ul class="disk">
+              <li> <img src="<?php echo $value["poster"] ?>" alt=""> </li>
+              <li> Title: <?php echo $value["title"] ?> </li>
+              <li> Author: <?php echo $value["author"] ?> </li>
+              <li> Year: <?php echo $value["year"] ?> </li>
+            </ul>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </main>
+
+
+
 
   </body>
 </html>
